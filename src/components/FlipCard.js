2548a693e00e0f1ab60f7b341d872d38cc987367
ualiusman <usmanaliua.uali@gmail.com>
2024-06-11 14:01:47 +0500

    included flip logic

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import './FlipCard.css'
 
-function FlipCard({ cardFront, cardBack }) {
+function FlipCard({ cardFront, cardBack, rotation = 'x', flipEvent = 'onHover' }) {
 
     const [flipped, setFlipped] = useState(false);
 
@@ -10,13 +10,35 @@ function FlipCard({ cardFront, cardBack }) {
         setFlipped(!flipped);
     }
 
+    const getStyles = (rotation) => {
+        if (!flipped)
+            return;
+        if (rotation.toLowerCase() === "x") {
+            return {
+                transform: "rotateX(180deg)"
+            };
+        }
+        else {
+            return {
+                transform: "rotateY(180deg)"
+            };
+        }
+
+
+    }
+
     return (
         <div className="card-container">
-            <div onClick={() => flip()} className={flipped ? "card flipped" : "card"}>
+            <div style={getStyles(rotation)}
+                className={"card"}
+                onClick={() => flip()}
+                onMouseEnter={() => flip()}
+                onMouseLeave={() => flip()}
+            >
                 <div className="front">
                     {cardFront}
                 </div>
-                <div className="front back">
+                <div style={getStyles(rotation)} className="front back">
                     {cardBack}
                 </div>
             </div>

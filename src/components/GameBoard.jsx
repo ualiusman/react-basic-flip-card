96ab57d9baafe7b93c4ed64bdbd05e779f5616aa
ualiusman <usmanaliua.uali@gmail.com>
2024-06-14 13:13:46 +0500

    game board logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,97 @@
-import react from 'react'
+import react, { useState } from 'react'
+import GameOver from './GameOver';
+import Card from './Card';
 
 const GameBoard = () => {
 
+    const cards = [
+        "TV",
+        "TV",
+        "VT",
+        "VT",
+        "hbird",
+        "hbird",
+        "name",
+        "name",
+        "seal",
+        "seal",
+        "tracks",
+        "tracks"
+    ];
+
+
+
+    const shuffle = (array) => {
+        let currentIndex = array.length,
+            temporaryValue,
+            randomIndex;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+
+        }
+
+        return array;
+    }
+
+
+    const [cardList, setCardList] = useState(
+        shuffle(cards).map((name, index) => {
+            return {
+                id: index,
+                name: name,
+                flipped: false,
+                matched: false,
+            }
+        })
+    );
+
+    const [flippedCards, setFlippedCards] = useState([]);
+    const [gameOver, setGameOver] = useState(false);
+
+
+
+    const handleClick = (name, index) => {
+
+    }
+
+
+    const check = () => {
+
+    }
+
+    const isGameOver = () => {
+
+    }
+
+
+    const restartGame = () => {
+
+    }
+
+
+    return (
+
+        <div className='game-board'>
+            {!gameOver &&
+                cardList.map((card, index) => {
+                    <Card
+                        key={index}
+                        id={index}
+                        name={card.name}
+                        flipped={card.flipped}
+                        matched={card.matched}
+                        clicked={flippedCards.length == 2 ? () => { } : handleClick()}
+                    />
+                })}
+            {gameOver && <GameOver restartGame={restartGame} />}
+        </div>
+    );
+
 }
+
+export default GameBoard;

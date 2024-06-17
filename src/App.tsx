7a23efa362dfee2856c7147e2ab34c7a5bc0bb78
ualiusman <usmanaliua.uali@gmail.com>
2024-06-17 19:00:26 +0500

    changes

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import NewGame from "./components/NewGame";
+import { BrowserRouter, Route, } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      one and one
+    </>
+  );
+}
+
+
+
+
+export default App;
